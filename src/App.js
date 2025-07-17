4b3d75dcaec4933d266a95a1b5755b98227940fa
scattman105 <c.zhang@me.com>
2025-07-17 02:47:49 -0500

    commit msg

--- src/App.js
+++ src/App.js
@@ -5,10 +5,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <title>Let's get LINKED!</title>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>this is hard-coded HTML</p>
+          <br/>
+          <p classname="React-text"></p>
         <a
           className="App-link"
           href="https://www.linkedin.com/in/charlie-zhang-profile"
